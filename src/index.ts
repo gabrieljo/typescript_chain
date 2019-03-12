7041308a7cb7c3819f36fbdd11ecea11f5fb010f
Gabriel Jo <gabrieljo1302@gmail.com>
2019-03-12 22:47:59 +1300

    set block class

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,48 @@
-const person = (name: string, age: number, gender: string): string => {
-  return `Hello ${name}, you are ${age}, you are a ${gender}`;
-};
+import * as CryptJS from "crypto-js";
 
-const result = person("Gabriel", 38, "male");
-console.log(result);
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+
+  static claculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string =>
+    CryptJS.SHA256(index + previousHash + timestamp + data).toString();
+
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.timestamp = timestamp;
+  }
+}
+
+const genesisBlock: Block = new Block(
+  0,
+  "#@AQSEDFASDFQWE!@#$",
+  "",
+  "hello",
+  123456
+);
+
+let blockchain: Block[] = [genesisBlock];
+
+const getBlockchain = (): Block[] => blockchain;
+
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+console.log(blockchain);
